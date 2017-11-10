d46ca56a174be0a9bdcac09a88dd81a8473c6302
Andrew Dansby <andydansby@gmail.com>
2017-11-10 04:39:54 -0500

    Delete extern_e.h

--- dev/extern_e.h
+++
@@ -1,8 +0,0 @@
-// MT Engine MK2
-// Copyleft 2014 the Mojon Twins
-
-// extern_e.h
-// Extended External custom code to be run from a script
-
-void do_extern_action (unsigned char n, unsigned m) {
-}
