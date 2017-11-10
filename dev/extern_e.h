1e4b780950ac8cfd457ee05b0da6c72854363834
Andrew Dansby <andydansby@gmail.com>
2017-11-10 04:40:05 -0500

    Add files via upload

---
+++ dev/extern_e.h
@@ -0,0 +1,8 @@
+// MT Engine MK2
+// Copyleft 2014 the Mojon Twins
+
+// extern_e.h
+// Extended External custom code to be run from a script
+
+void do_extern_action (unsigned char n, unsigned m) {
+}
